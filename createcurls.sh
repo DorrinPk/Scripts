27fb00f8578b547191265c02a3eb07503e3a8d32
Dorrin <DorrinPk@users.noreply.github.com>
2016-08-25 10:19:02 -0400

    first commit

---
+++ createcurls.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script is made to set up my API calls ready for me
+# I originally made this because I can't have dynamic vars in JSON
+# yeah I could have wrote this in something else
+# don't judge me ^_^
+# this is ran after my clonerepos.sh script
+
+jsonhead="curl -u "\""USER:PASS"\"" https://api.github.com/orgs/eltorocorp/repos -d "\'"{"\""name"\"":"\"""
+jsontail=""\"","\""private"\"":""\""true""\""}"\'""
+echo "#!/bin/bash" >> runcurls.sh
+echo '\n' >> runcurls.sh
+for name in $(cat newreponame.txt)
+ do
+   echo $jsonhead$name$jsontail >> runcurls.sh
+
+ done
